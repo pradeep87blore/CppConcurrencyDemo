70d5d1f595a6b55e07451383418793642fdd8396
Pradeep B <{ID}+{username}@users.noreply.github.com>
2021-09-23 20:30:13 +0530

    Updating as per exercise 3 (work queues)

--- ShipSailExercise/Captain.cpp
+++ ShipSailExercise/Captain.cpp
@@ -1,86 +1,84 @@
 
 #include <iostream>
 #include <thread>
-
+#include <queue>
 using namespace std;
 
-void Clean()
-{
-	int cleanCounter = 10;
-
-	while (cleanCounter--)
-	{
-		this_thread::sleep_for(chrono::milliseconds(1000));
-		cout << "[" << this_thread::get_id() << "] : Cleaning" << endl;
-	}
-
-	cout << "[" << this_thread::get_id() << "] : Done with Cleaning" << endl;
-}
+queue<int> speedQueue, cleanQueue;
 
 void FullSpeedAhead()
 {
-	int speedCounter = 3;
 
-	while (speedCounter--)
+	while (true)
 	{
-		this_thread::sleep_for(chrono::milliseconds(1000));
-		cout << "[" << this_thread::get_id() << "] : Speeding up" << endl;
+		if (speedQueue.empty())
+		{
+			cout << "[" << this_thread::get_id() << "] [ Speed Queue ] : Nothing to do, sleeping" << endl;
+			this_thread::sleep_for(chrono::milliseconds(2000));
+		}
+		else
+		{
+			cout << "[" << this_thread::get_id() << "] [ Speed Queue ] : Speeding up" << endl;
+			speedQueue.pop();
+			this_thread::sleep_for(chrono::milliseconds(1000));
+		}
 	}
-
-	cout << "[" << this_thread::get_id() << "] : At max speed" << endl;
 }
 
-void StopEngine()
+void Clean()
 {
-	int stopCounter = 3;
 
-	while (stopCounter--)
+	while (true)
 	{
-		this_thread::sleep_for(chrono::milliseconds(1000));
-		cout << "[" << this_thread::get_id() << "] : Slowing down" << endl;
+		if (cleanQueue.empty())
+		{
+			cout << "[" << this_thread::get_id() << "] [ Clean Queue ] : Nothing to do, sleeping" << endl;
+			this_thread::sleep_for(chrono::milliseconds(2000));
+		}
+		else
+		{
+			cout << "[" << this_thread::get_id() << "] [ Clean Queue ] : Cleaning" << endl;
+			cleanQueue.pop();
+			this_thread::sleep_for(chrono::milliseconds(1000));
+		}
 	}
-
-	cout << "[" << this_thread::get_id() << "] : Stopped" << endl;
 }
 
 
 int main()
 {
 	// Captain can give three orders: Clean, Full Speed ahead and Stop Engine
-	// Captain doesnt have to wait for the Clean command to complete to continue other activities
-	// Full Speed Ahead and Stop Engine are blocking calls
+	// Captain doesnt have to wait for the commands to complete to continue other activities
 
 	int command;
+
+	thread speedThread(FullSpeedAhead);
+	thread cleanThread(Clean);
+
+	speedThread.detach();
+	cleanThread.detach();
 
 	while (true)
 	{
 		cout << " Enter the command: " << endl;
-		cout << "1: Cleaning\n2: Full Speed Ahead\n3: Stop the engine" << endl;
+		cout << "1: Cleaning\n2: Full Speed Ahead\n100: Exit program" << endl;
 		cin >> command;
 		switch (command)
 		{
 			case 1:
 			{
-				thread cleanerThread(Clean);
-				cleanerThread.detach();
+				speedQueue.push(1);
 			}
 			break;
 			case 2:
 			{
-				thread speedThread(FullSpeedAhead);
-				speedThread.join();
-			}
-			break;
-			case 3:
-			{
-				thread stopThread(StopEngine);
-				stopThread.join();
+				cleanQueue.push(1);
 			}
 			break;
 			case 100:
 			{
 				cout << "Exitting program\n\n\n";
-				break;
+				return 0;
 			}
 			default:
 			{
@@ -88,6 +86,6 @@ int main()
 			}
 
 		}
-
+		this_thread::sleep_for(chrono::milliseconds(5000)); // Wait for 5 seconds before the next command
 	}
 }
